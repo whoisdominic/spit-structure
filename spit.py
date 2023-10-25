a487954d75f44a43ad5b1bb2e5a5235c1dcb09b4
Dominic <dominicshajaa@icloud.com>
2023-10-24 22:22:52 -0700

    Updated script with a verbose flag

--- spit.py
+++ spit.py
@@ -15,14 +15,14 @@ default_ignored = {
     "DerivedData"
 }
 
-def print_directory_structure(path, depth=None, ignored=None, indent=0):
+def print_directory_structure(path, depth=None, ignored=None, verbose=False, indent=0):
     """Recursively prints the directory structure."""
     # If depth is zero, we won't proceed
     if depth == 0:
         return
     
     # Check if the directory or file is in the ignored list
-    if os.path.basename(path) in ignored:
+    if not verbose and os.path.basename(path) in ignored:
         return
 
     # Base case: If path is a file, print its name
@@ -35,12 +35,13 @@ def print_directory_structure(path, depth=None, ignored=None, indent=0):
     for item in sorted(os.listdir(path)):
         # If depth is not None, decrement it for the next level
         next_depth = depth - 1 if depth is not None else None
-        print_directory_structure(os.path.join(path, item), next_depth, ignored, indent + 1)
+        print_directory_structure(os.path.join(path, item), next_depth, ignored, verbose, indent + 1)
 
 def main():
     # Set up argument parsing
     parser = argparse.ArgumentParser(description="Print directory structure.")
     parser.add_argument('-depth', '-d', type=int, default=None, help="Max depth to traverse. Default is infinite depth.")
+    parser.add_argument('-verbose', '-v', action='store_true', help="Verbose mode. Ignores all default ignored directories and files.")
     parser.add_argument('-ignore', '-i', nargs='*', default=[], help="Names of additional directories or files to ignore.")
     args = parser.parse_args()
 
@@ -48,7 +49,7 @@ def main():
     ignored = default_ignored.union(args.ignore)
 
     # Call the function with the specified depth and ignored set
-    print_directory_structure(os.getcwd(), args.depth, ignored)
+    print_directory_structure(os.getcwd(), args.depth, ignored, args.verbose)
 
 if __name__ == "__main__":
     main()
